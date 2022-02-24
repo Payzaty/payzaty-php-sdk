2b5dc9695e6fb3310068e03c534d98c5700d9cab
Payzaty <90286764+Payzaty@users.noreply.github.com>
2022-02-24 05:06:47 +0200

    Update StatusResponse.php

--- src/Response/StatusResponse.php
+++ src/Response/StatusResponse.php
@@ -18,12 +18,12 @@ class StatusResponse
     public function __construct($data)
     {
         $this->success = (bool)$data->success;
-        $this->paid = (bool)$data->paid;
-        $this->status = $data->status;
+        $this->paid = (bool)$data->paid ?? false;
+        $this->status = $data->status ?? '';
         $this->udf1 = $data->udf1;
         $this->udf2 = $data->udf2;
         $this->udf3 = $data->udf3;
-        $this->error = $data->error;
-        $this->errorText = $data->errorText;
+        $this->error = $data->error ?? '';
+        $this->errorText = $data->errorText ?? '';
     }
 }
